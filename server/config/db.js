1e414e3b47a4c23fb50067f556bd5f4fbf417ed2
Hassan Javed <115964893+hjaved09@users.noreply.github.com>
2022-10-30 21:13:02 -0400

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+
+  //MongoDB Atlas Server
+  "URI": "mongodb+srv://hassan:JXMduNOvqxVTioGG@mondodbserver.celr4bm.mongodb.net/?retryWrites=true&w=majority"
+}
